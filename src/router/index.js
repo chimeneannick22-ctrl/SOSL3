83bfd63c3b8f8cc3f7ce52f8617e0a4ce8dbeeb3
chimeneannick22-ctrl <chimeneannick22@gmail.com>
2026-06-01 20:55:15 +0845

    Added Changes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router"
 
-import Home from "../views/Home.vue"
+import Home from "../views/home.vue"
 import About from "../views/About.vue"
 import Services from "../views/Services.vue"
 
